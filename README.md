64e8ea21c0a9e231b8b8bb48cbaa0618b64dbf92
Stephen Jenkinson <stephen.Jenkinson@au.abb.com>
2015-08-20 21:17:08 +1000

    Change script to pick up files from current directory
    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # GettingAndCleaningData
 Coursera Getting and Cleaning Data Course project
+
+## Contents 
+* run_analysis.R: R Script that process Samsung activity data into a tidy data set
+* Readme.md: Details on the content for the course project and instructions
+* codebook.md: Code Book that describes the variables and summaries calculated
+
+
+## run_analysis.R
+### Requirements
+The R working directory should be set to the run_analysis.R source file location
+
+The following files are expected in the working directory
+
+ * UCI HAR Dataset/train/X_train.txt
+ * UCI HAR Dataset/test/X_test.txt
+ * UCI HAR Dataset/train/y_train.txt
+ * UCI HAR Dataset/test/y_test.txt
+ * UCI HAR Dataset/features.txt
+ * UCI HAR Dataset/activity_labels.txt
+
+### How the script works 
+
+__Step 1: Read and merge__
+
+Read in the training and test sets and merge to create one data set
+
+__Step 2: Apply column names__ 
+
+Read in the features.txt file and apply it as the the column names for the dataset
+
+__Step 3: set descriptive activity names__ 
+
+Read in activity labels and replace the description of the activity name over the matching column values (i.e. 1 - 6)
+
+__Step 4: Create tidy dataset__ 
+
+Using dplyr, summarise each column with the mean of all values grouping by the Activity
+
+__Step 5: Write tidy tidy dataset to file__
+
+Write the tidy dataset out as a txt file for submission
+
--- run_analysis.R
+++ run_analysis.R
@@ -2,25 +2,25 @@ library(dplyr)
 library(stringr)
 
 #merge training and test sets to create one data set
-train_X <- read.table('../dataset/train/X_train.txt', header=F, fill=T)
-test_X <- read.table('../dataset/test/X_test.txt', header=F, fill=T)
+train_X <- read.table('UCI HAR Dataset/train/X_train.txt', header=F, fill=T)
+test_X <- read.table('UCI HAR Dataset/test/X_test.txt', header=F, fill=T)
 dirty_ds <- merge(train_X, test_X,all = TRUE)
 
 #name columns
-cols <- read.table('../dataset/features.txt', header=F, fill=T)
+cols <- read.table('UCI HAR Dataset/features.txt', header=F, fill=T)
 colnames(dirty_ds) <- cols[,2]
 
 #extract measurements for mean and standard deviation only
 dirty_ds <- dirty_ds[,grep("mean\\(|std\\(", names(dirty_ds))]
 
 #include activy into the dirty data set
-y_train <- read.table('../dataset/train/y_train.txt', header=F, fill=T)
-y_test <- read.table('../dataset/test/y_test.txt', header=F, fill=T)
+y_train <- read.table('UCI HAR Dataset/train/y_train.txt', header=F, fill=T)
+y_test <- read.table('UCI HAR Dataset/test/y_test.txt', header=F, fill=T)
 y_merge <- rbind(y_train, y_test)
 dirty_ds <- cbind(y_merge, dirty_ds)
 
-#use descriptive activity names 
-dsLabels <- read.table('../dataset/activity_labels.txt', header=F, fill=T)
+#set descriptive activity names 
+dsLabels <- read.table('UCI HAR Dataset/activity_labels.txt', header=F, fill=T)
 activityName <- dsLabels[,2]
 dirty_ds[,1] <- activityName[dirty_ds[,1]]
 colnames(dirty_ds)[1] <- 'Activity'
